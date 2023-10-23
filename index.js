3be3000d6e176c115ff706bf7d6a64bf3a877db5
jeffrey domond <noella@Noellas-iMac.local>
2023-10-22 21:58:11 -0400

    looping code

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  countDown(10);
+  
+  module.exports = { writeCards, countDown };
+  
